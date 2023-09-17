0825fc464c71964451d3f9837aa7a886dc0ba022
Andrii Chychkan <mywork1728@gmail>
2023-09-17 22:17:52 +0300

    added persist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { GlobalStyle } from 'GlobalStyle';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-      <GlobalStyle />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <GlobalStyle />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
